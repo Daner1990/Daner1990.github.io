90a57139518e101548cf635da87c335f1fe92a51
Daner1990 <lilinjin19901125@163.com>
2015-10-08 18:25:32 +0800

    up

--- javascript/main.js
+++ javascript/main.js
@@ -18,10 +18,28 @@ require(['events','menus'],function(events,menus){
 
     //menu init
     //menus.initMenu();
-    document.getElementById('page_tags_list').addEventListener('click',function(e){
-        if(e.target && e.target.nodeName.toLowerCase() == 'span'){
-            menus.filter(e);
-        }
-    },false);
+    var tagsList = document.getElementById('page_tags_list');
+    if(tagsList){
+        tagsList.addEventListener('click',function(e){
+            if(e.target && e.target.nodeName.toLowerCase() == 'span'){
+                menus.filter(e);
+            }
+        },false);
+    }
+
+    //categories redirect
+    var categoriesList = document.getElementById('page_categories_list');
+    if(categoriesList){
+        categoriesList.addEventListener('click',function(e){
+            var hash = "";
+            if(e.target && e.target.nodeName.toLowerCase() == 'span'){
+                hash = e.target.innerHTML;
+            }
+            if(hash){
+                window.location.href= window.location.host + window.location.path + "#" + hash;
+            }
+            return false;
+        });
+    }
 
 });
