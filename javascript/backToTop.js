1fa081974ebf5dfc4819ad28fbb55fe038b3df25
Daner1990 <lilinjin19901125@163.com>
2015-10-10 18:58:59 +0800

    up

--- javascript/backToTop.js
+++ javascript/backToTop.js
@@ -9,20 +9,13 @@ define(function(){
         var documentHeight = document.body.clientHeight;
         var scrollTopHeight = document.body.scrollTop;
         var backToTopBtn = document.getElementsByClassName('backtotop')[0];
-        
-        if(!backToTopBtn){
-            backToTopBtn.style.opacity =0;
-            return false;
-        }
-
-        if(clientHeight >= documentHeight){
-            backToTopBtn.style.opacity =0;
-            return false;
-        }
 
         if(clientHeight < documentHeight && scrollTopHeight > 100){
             backToTopBtn.style.opacity = 1;
             //backToTopBtn.setAttribute("style","opacity:1");
+            return false;
+        }else{
+            backToTopBtn.style.opacity =0;
             return false;
         }
     };
